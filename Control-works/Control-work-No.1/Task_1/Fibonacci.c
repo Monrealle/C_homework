f86bf8f2d0763d1c015b8deeaac8b578ddeb595d
Stanislav Klimovich <stasklimovish@gmail.com>
2025-10-20 16:56:35 +0300

    update Fibonacci.c

--- Control-works/Control-work-No.1/Task_1/Fibonacci.c
+++ Control-works/Control-work-No.1/Task_1/Fibonacci.c
@@ -4,57 +4,57 @@
 
 int fibonacci(int count)
 {
-	int a = 1;
-        int b = 1;
-	int c = 0;
-        int n = 3;
+    int a = 1;
+    int b = 1;
+    int c = 0;
+    int n = 3;
 
-	while(n <= count)
-        {
-	        c = a + b;
-        	a = b;
-	        b = c;
-        	n++;
-	}
+    while(n <= count)
+    {
+        c = a + b;
+       	a = b;
+        b = c;
+       	n++;
+    }
 
-	return c;
+    return c;
 }
 
 
 int main(void)
 {
-	int number;
+    int number;
 
-	printf("Введите n: ");
-	scanf("%d", &number);
+    printf("Введите n: ");
+    scanf("%d", &number);
 
-	if (number < 0)
-	{
-		printf("Число n не должно быть отритцательнвм\n");
-	}
+    if (number < 0)
+    {
+	printf("Число n не должно быть отритцательнвм\n");
+    }
 
-	else if (number == 0)
-	{
-		printf("Нулевого числа Фибоначи не существует\n");
-	}
+    else if (number == 0)
+    {
+	printf("Нулевого числа Фибоначи не существует\n");
+    }
 
-	else if (number == 1 | number == 2)
-	{
-		printf("Число %d: 1\n", number);
-	}
+    else if (number == 1 | number == 2)
+    {
+	printf("Число %d: 1\n", number);
+    }
 
-	else
-	{
-		if (number >= 47)
-		{
-			printf("Результат ниже может быть аномальным из-за использования типа int:\n");
-		}
+    else
+    {
+	if (number >= 47)
+        {
+	    printf("Результат ниже может быть аномальным из-за использования типа int:\n");
+        }
 
-		int result = fibonacci(number);
-		printf("Число %d: %d\n", number, result);
-	}
+	int result = fibonacci(number);
+	printf("Число %d: %d\n", number, result);
+    }
 
-	return 0;
+    return 0;
 }
 
 
