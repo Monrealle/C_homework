cbb0b34ac3b85bb81fe3ebc34e0e6889e8f79637
Stanislav Klimovich <stasklimovish@gmail.com>
2025-12-03 01:12:53 +0000

    [200~clang-format в CI

--- 2.Style-guide/Task_2/Incomplete-quotient.c
+++ 2.Style-guide/Task_2/Incomplete-quotient.c
@@ -1,33 +1,34 @@
 #include <stdio.h>
 
-int main(void)
+int
+main (void)
 {
     int number_1, number_2;
     int result = 0;
-    printf("Введите делимое: \n");
-    scanf("%i", &number_1);
-    printf("Введите делитель \n");
-    scanf("%i", &number_2);
+    printf ("Введите делимое: \n");
+    scanf ("%i", &number_1);
+    printf ("Введите делитель \n");
+    scanf ("%i", &number_2);
     int examination = number_1 % number_2;
 
-    if(examination != 0)
+    if (examination != 0)
     {
-        if(number_2 > number_1)
+        if (number_2 > number_1)
         {
-            printf("Делитель больше делимого\n");
+            printf ("Делитель больше делимого\n");
         }
 
         else
         {
-            
-            while(number_1 >= number_2)
+
+            while (number_1 >= number_2)
             {
                 number_1 = number_1 - number_2;
                 result += 1;
             }
 
-            printf("Неполное частное: %d\n", result);
-            printf("Остаток от деления %d\n", examination);
+            printf ("Неполное частное: %d\n", result);
+            printf ("Остаток от деления %d\n", examination);
         }
     }
 
@@ -35,7 +36,7 @@ int main(void)
     {
         number_1 = number_1 - number_2;
         result += 1;
-        printf("Частное: %i\n", result);
+        printf ("Частное: %i\n", result);
     }
 
     return 0;
--- 2.Style-guide/Task_4/Flipping-the-array.c
+++ 2.Style-guide/Task_4/Flipping-the-array.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 
-int main(void)
+int
+main (void)
 {
     int number_m;
     int number_n;
-    printf("Введите число m (До 10000)\n");
-    scanf("%i", &number_m);
-    printf("Введите число n (До 10000)\n");
-    scanf("%i", &number_n);
+    printf ("Введите число m (До 10000)\n");
+    scanf ("%i", &number_m);
+    printf ("Введите число n (До 10000)\n");
+    scanf ("%i", &number_n);
 
-    if(number_n < number_m)
+    if (number_n < number_m)
     {
-        printf("Ошибка: число m больше числа n\n");
+        printf ("Ошибка: число m больше числа n\n");
     }
 
     else
@@ -20,43 +21,41 @@ int main(void)
         int numbers[10000];
         int total = number_m + number_n;
 
-        for (int i = 0; i <= total; i++) 
+        for (int i = 0; i <= total; i++)
         {
             numbers[i] = i;
         }
 
-        for (int i = 0; i <= number_n/2; i++) 
+        for (int i = 0; i <= number_n / 2; i++)
         {
             int temp = numbers[i];
-            numbers[i] = numbers[number_n-i];
-            numbers[number_n-i] = temp;
+            numbers[i] = numbers[number_n - i];
+            numbers[number_n - i] = temp;
         }
-    
+
         int segment_size = number_n - number_m + 1;
-        for (int i = 0; i < segment_size/2; i++) 
+        for (int i = 0; i < segment_size / 2; i++)
         {
             int temp = numbers[i];
-            numbers[i] = numbers[segment_size-1-i];
-            numbers[segment_size-1-i] = temp;
+            numbers[i] = numbers[segment_size - 1 - i];
+            numbers[segment_size - 1 - i] = temp;
         }
-            
-        for (int i = 0; i < number_m/2; i++) 
+
+        for (int i = 0; i < number_m / 2; i++)
         {
-            int temp = numbers[segment_size+i];
-            numbers[segment_size+i] = numbers[number_n-i];
-            numbers[number_n-i] = temp;
+            int temp = numbers[segment_size + i];
+            numbers[segment_size + i] = numbers[number_n - i];
+            numbers[number_n - i] = temp;
         }
-            
-        printf("\nПосле: ");
-        for (int i = 0; i <= number_n; i++) 
+
+        printf ("\nПосле: ");
+        for (int i = 0; i <= number_n; i++)
         {
-            printf("%d ", numbers[i]);
+            printf ("%d ", numbers[i]);
         }
-        
-        printf("\n");
+
+        printf ("\n");
     }
 
-    return 0; 
-
-
+    return 0;
 }
