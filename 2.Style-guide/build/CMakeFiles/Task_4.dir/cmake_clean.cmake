4e545677661e19dd1c75e5d2a65990ff5a7e5fee
Stanislav Klimovich <stasklimovish@gmail.com>
2025-12-03 17:22:16 +0000

    CMake for homework 2

---
+++ 2.Style-guide/build/CMakeFiles/Task_4.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Task_4.dir/src/Task_4/Flipping-the-array.c.o"
+  "CMakeFiles/Task_4.dir/src/Task_4/Flipping-the-array.c.o.d"
+  "Task_4"
+  "Task_4.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Task_4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
