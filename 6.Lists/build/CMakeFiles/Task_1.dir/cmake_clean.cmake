247ca13467a213ee906cf52fee7e86b8d4869aad
Stanislav Klimovich <stasklimovish@gmail.com>
2025-12-03 02:43:14 +0000

    Update CMake

---
+++ 6.Lists/build/CMakeFiles/Task_1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Task_1.dir/src/Task_1/Sorted-list.c.o"
+  "CMakeFiles/Task_1.dir/src/Task_1/Sorted-list.c.o.d"
+  "Task_1"
+  "Task_1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Task_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
